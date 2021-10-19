56cb28a6083b19cd713131a3b0f87cd465d6ceae
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-10-18 21:09:27 -0500

    add gem, add controller, add restful routes for index, show, and create.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,25 @@
 class ProductsController < ApplicationController
   def index
-    products = Product.all
-    render json: products.as_json
+    all_products = Product.all
+    render json: all_products.as_json
   end
 
+  def show
+    the_id = params[:id]
+    product_by_id = Product.find_by(id: the_id)
+    render json: product_by_id.as_json
+  end
 
+  def create
+    new_product = Product.new(
+  
+      name: params[:name], 
+      price: params[:price], 
+      img_url: params[:url], 
+      description: params[:description]
+    
+    )
+    new_product.save
+    render json: new_product.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/product" => "products#index"
+  get "/products" => "products#index"
+  get "/products/:id" => "products#show"
+  post "/products" => "products#create"
   
+
 end
